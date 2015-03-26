e0dad7d82349260b0326ee9b9231a72e60e96eba
Jacob McKinney <jmckin02@gmail.com>
2015-03-25 22:15:26 -0400

    Create ppl-list.php

---
+++ ppl-list.php
@@ -0,0 +1,18 @@
+<?php
+//Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+?>
+
+<div  class="ppl-wrapper">
+
+	<div class="ppl-group">
+
+		<img class="ppl-img" src="" href="" />
+
+		<p class="ppl-name">Name</p>
+		<p class="ppl-web">http://</p>
+		<p class="ppl-info">Bio</p>
+
+	</div>
+
+</div>
