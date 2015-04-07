80a950ac123128d56e30b7c795534738d756bb96
Jacob McKinney <jmckin02@gmail.com>
2015-04-07 15:02:17 -0400

    Update ppl-post-type.php
    Reorganized parameters for register_post_type() as well as added some parameters for 'labels' and 'supports'.

--- ppl-post-type.php
+++ ppl-post-type.php
@@ -5,19 +5,32 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Register People custom post type
 function people_post_type() {
-	register_post_type( 
-		'ppl_people',
-		array(
-			'labels' => array(
-				'name' => __( 'People' ),
-				'singular_name' => __( 'Person' )
-				),
+
+	$labels = array(
+				'name' => 'People',
+				'singular_name' => 'People',
+				'add_new' => 'Add People',
+				'all_items' => 'All People',
+				'add_new_item' => 'Add People',
+				'edit_item' => 'Edit People',
+				'new_item' => 'New People',
+				'view_item' => 'View People',
+				'search_items' => 'Search People',
+				'not_found' => 'No People Found',
+				'not_found_in_trash' => 'No People were hiding in the trash'
+				);
+
+	$supports = array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields' );
+
+	$args = array(
+			'labels' => $labels,
 			'public' => true,
 			'rewrite' => array( 'slug' => 'people' ),
 			'menu_icon' => 'dashicons-id-alt',
-			'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields' )
-			) 
-		);
+			'supports' => $supports
+			);
+
+	register_post_type( 'ppl_people', $args );
 }
 
 add_action( 'init', 'people_post_type' );
