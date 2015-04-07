6736007266ae5c3256ceb017b1437842c777c496
Jacob McKinney <jmckin02@gmail.com>
2015-04-07 13:56:40 -0400

    Update admin.php
    Changed some of the format of ppl_option_page() and added the beginning of the settings page.

--- admin.php
+++ admin.php
@@ -3,14 +3,23 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-//Render admin page
-function ppl_render_admin() {
-	echo 'Sample admin area';
-}
-
 //Add menu to dashboard
 function ppl_add_menu() {
-	add_menu_page( 'People', 'People', 'manage_options', 'people', 'ppl_render_admin', false, 26 );
+	add_menu_page( 'People', 'People', 'manage_options', 'people', 'ppl_options_page', false, 26 );
 }
 
 add_action( 'admin_menu', 'ppl_add_menu' );
+?>
+
+<?php 
+//Render admin page
+function ppl_options_page() {
+?>
+
+<div class="wrap">
+	<h2>People User Settings</h2>
+	<p>Options related to the people plugin.</p>
+</div>
+
+
+<?php } ?>
